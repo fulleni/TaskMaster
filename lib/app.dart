a62da99a5114f56c9ebef3d8355dfd90c02ee6a2
fulleni <fulleni@hotmail.com>
2025-01-27 14:50:16 +0100

    Refactor project structure to replace 'todos' with 'taskmaster' in import paths

--- lib/app.dart
+++ lib/app.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:todos/home/view/home_page.dart';
+import 'package:taskmaster/home/view/home_page.dart';
 import 'package:todos_repository/todos_repository.dart';
 
 class App extends StatelessWidget {
--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:local_storage_todos_client/local_storage_todos_client.dart';
 import 'package:shared_preferences/shared_preferences.dart';
-import 'package:todos/app.dart';
+import 'package:taskmaster/app.dart';
 import 'package:todos_repository/todos_repository.dart';
 
 void main() async {
@@ -14,32 +14,9 @@ void main() async {
 
   final todosRepository = TodosRepository(todosClient: todosClient);
 
-  runApp(MyApp());
-}
-
-class MyApp extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'TaskMaster',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: MyHomePage(),
-    );
-  }
-}
-
-class MyHomePage extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('TaskMaster'),
-      ),
-      body: Center(
-        child: Text('Hello, world!'),
-      ),
-    );
-  }
+  runApp(
+    App(
+      todosRepository: todosRepository,
+    ),
+  );
 }
