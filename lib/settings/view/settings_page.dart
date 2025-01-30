f89a116f1a43e87dd783e0ed1ffb25ef566ef7f8
fulleni <fulleni@hotmail.com>
2025-01-30 12:25:26 +0100

    feat(settings): about page.

--- lib/settings/view/settings_page.dart
+++ lib/settings/view/settings_page.dart
@@ -7,14 +7,18 @@ class SettingsPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final theme = Theme.of(context);
+    final textColor =
+        theme.brightness == Brightness.light ? Colors.black : Colors.white;
+
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Settings'),
+        title: Text('Settings', style: TextStyle(color: textColor)),
       ),
       body: ListView(
         children: [
           ListTile(
-            title: const Text('Theme Settings'),
+            title: Text('Theme', style: TextStyle(color: textColor)),
             onTap: () {
               Navigator.push(
                 context,
@@ -25,12 +29,29 @@ class SettingsPage extends StatelessWidget {
             },
           ),
           ListTile(
-            title: const Text('Font Settings'),
+            title: Text('Font', style: TextStyle(color: textColor)),
             onTap: () {
               Navigator.push(
                 context,
                 MaterialPageRoute(
                     builder: (context) => const FontSettingsPage()),
+              );
+            },
+          ),
+          ListTile(
+            title: Text('About', style: TextStyle(color: textColor)),
+            onTap: () {
+              showAboutDialog(
+                context: context,
+                // applicationIcon: const Icon(Icons.task),
+                // applicationName: 'TaskMaster',
+                applicationVersion: '0.13.0',
+                children: [
+                  Text(
+                    'TaskMaster is a user-friendly task management application built to help you stay organized and productive.',
+                    style: TextStyle(color: textColor),
+                  ),
+                ],
               );
             },
           ),
